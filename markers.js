fbd4c1ee801bc9dbb106df8819086dea8ceb5c8a
Jennifer Cen <cenjennifer@gmail.com>
2016-06-23 17:33:16 -0400

    making markers

---
+++ markers.js
@@ -0,0 +1,34 @@
+const markers = {
+    {
+        'id': 1,
+        'image': url
+    },
+    {
+        'id': 2,
+        'image': url
+    },
+    {
+        'id': 3,
+        'image': url
+    },
+    {
+        'id': 4,
+        'image': url
+    },
+    {
+        'id': 5,
+        'image': url
+    },
+    {
+        'id': 6,
+        'image': url
+    },
+    {
+        'id': 7,
+        'image': url
+    },
+    {
+        'id': 8,
+        'image': url
+    }
+}
