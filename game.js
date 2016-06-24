81216a6a3373488a3e0d582a9a5d8116649abe45
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2016-06-24 11:23:55 -0400

    Made Game Class

--- game.js
+++ game.js
@@ -2,17 +2,19 @@
 var _ = require('lodash');
 //GAME///
 
-class Game () {
-	this.count = 35;
-	this.board;
-	this.players = [];
-	this.activeSpace = [x,y] //The next space of the currentPlayer
-	this.currPlayer;   //ask turn for next player
-	this.deck = [];
-	this.dragon = "";// Player.Marker
+class Game {
+	constructor(){
+		this.count = 35;
+		this.board;
+		this.players = [];
+		this.activeSpace = [x,y] //The next space of the currentPlayer
+		this.currPlayer;   //ask turn for next player
+		this.deck = [];
+		this.dragon = "";// Player.Marker
+	}
 
 	//initializeGame: Generate new game, shuffle deck
-	initializeGame = function (numOfPlayers) {
+	initializeGame(numOfPlayers) {
 		// Generate a board
 		this.board = new Board();
 		// -	Create Players
@@ -23,12 +25,16 @@ class Game () {
 		// -	Deal Tiles
 		// TODO:  deal those tiles
 	};
-	//createPlayers - add players to game's player array
-	createPlayers = function (arguments) {
-		// 		Create turnOrderArray[player1, player2...]
 
+	//createPlayers - add players to game's player array
+	// function[players: Array] -> return turnOrderArray
+	createPlayers([players]) {
+		// 		Create turnOrderArray[player1, player2...]
+		this.players = players;
 		// - Players pick a start point on the board -> edge Point
 
 		// - Next Player picks a different start point (i.e. points must be unique)
 	};
+
+
 }
