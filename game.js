e7dcdd09329986fca6ac09fa6601320c8d89b4ad
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2016-06-25 20:10:03 -0400

    updating game with reset and start functions

--- game.js
+++ game.js
@@ -20,23 +20,35 @@ class Game {
 		this.players.length < 8 ? this.players.push(player): throw new Error "Room full" ;
 	};
 
-	//Run the game
-	run(){
-		this.turnOrderArray = getCanPlay(this.players);
-		//while more than 1 player on board and deck non empty -> play
-		var idx = 0; //turn pointer
-		while (count > 0 && this.turnOrderArray.length>1){
-			this.currPlayer = this.turnOrderArray[idx];
-			//TODO: game mechanics
-			
-			//move through turn array, wrap around when get to end
-			idx = (idx+1)%this.turnOrderArray.length
-			count --;
-		}
-	}
+	// //Run the game
+	// run(){
+	// 	this.turnOrderArray = getCanPlay(this.players);
+	// 	//while more than 1 player on board and deck non empty -> play
+	// 	var idx = 0; //turn pointer
+	// 	while (count > 0 && this.turnOrderArray.length>1){
+	// 		this.currPlayer = this.turnOrderArray[idx];
+	// 		//TODO: game mechanics
+	//
+	// 		//move through turn array, wrap around when get to end
+	// 		idx = (idx+1)%this.turnOrderArray.length
+	// 		count --;
+	// 	}
+	// }
 
-	start(){
+	//start with the players in the room
+	start(players){
+		// add players to the game
+		players.forEach((player)=>{
+			this.addPlayer(player)
+		})
+		//find all that can play
+		this.turnOrderArray = getCanPlay(this.players)
 		//for each player dealThree
+		this.turnOrderArray.forEach((player)=>{
+			player.tiles = this.deck.dealThree();
+		})
+
+		//TODO: how are we setting game master or player one?
 	}
 
 	getCurrentPlayer(){
@@ -47,9 +59,27 @@ class Game {
 		this.players.forEach((player)=>player.move())
 	}
 
-}
+	nextPlayer(){
 
+	}
+
+	//restart the game
+	reset(){
+		//retrieve all tiles
+		this.players.forEach(player=>{
+			//return player's tiles to the deck and shuffle
+			this.deck.reload(player.tiles).shuffle();
+			player.tiles=[];
+			//reset all players playability
+			player.canPlay = true;
+
+		})
+	}
+
+}
 /////END OF GAME CLASS/////
+
+
 //get Eligible players
 let getCanPlay = function(players){
 	players.filter((player) => {return player.canPlay})
