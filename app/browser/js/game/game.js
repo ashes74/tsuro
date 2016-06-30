cc37cfcbb3d3e41e85867ef53d0a330053d8a142
Kimber Lynch <kmlynch714@gmail.com>
2016-06-29 20:43:01 -0400

    fixed marker picking

--- app/browser/js/game/game.js
+++ app/browser/js/game/game.js
@@ -1,297 +1,306 @@
 tsuro.config(function ($stateProvider) {
-    $stateProvider.state('game', {
-        url: '/game/:gameName',
-        templateUrl: '/browser/js/game/game.html',
-        controller: 'gameCtrl'
-    });
+	$stateProvider.state('game', {
+		url: '/game/:gameName',
+		templateUrl: '/browser/js/game/game.html',
+		controller: 'gameCtrl'
+	});
 });
 
 tsuro.controller('gameCtrl', function ($scope, $firebaseAuth, firebaseUrl, $stateParams, $firebaseObject, $firebaseArray) {
-    var ref = firebase.database().ref();
-    var obj = $firebaseObject(ref);
+	var ref = firebase.database().ref();
+	var obj = $firebaseObject(ref);
 
-    var gameRef = ref.child('games').child($stateParams.gameName);
-    var deckRef = gameRef.child('initialDeck');
-    var playersRef = gameRef.child('players');
-    var markersRef = gameRef.child('availableMarkers');
+	var gameRef = ref.child('games').child($stateParams.gameName);
+	var deckRef = gameRef.child('initialDeck');
+	var playersRef = gameRef.child('players');
+	var markersRef = gameRef.child('availableMarkers');
 
-    // intialize game
-    $scope.game = new Game($stateParams.gameName, $stateParams.deck);
+	// intialize game
+	$scope.game = new Game($stateParams.gameName, $stateParams.deck);
+	$scope.game.deck = $firebaseObject(deckRef);
 
-    $scope.game.deck = $firebaseObject(deckRef);
+	var markersArr = $firebaseArray(markersRef);
 
-    var markersArr = $firebaseArray(markersRef);
+	markersArr.$loaded().then(function (data) {
+		$scope.game.availableMarkers = data[0];
+		$scope.game.availableMarkers = $scope.game.availableMarkers.filter(function(elem){
+			return typeof elem === "string";
+		});
+	});
 
-    markersArr.$loaded().then(function (data) {
-        $scope.availableMarkers = data[0]
-    });
+	markersRef.on('child_changed',function (data) {
+		$scope.game.availableMarkers = data.val();
+	});
 
-    markersRef.on('value', function (availableMarkers) {
-        $scope.availableMarkers = Object.keys(availableMarkers).map(function (i) {
-            return availableMarkers[i];
-        });
-    });
+	firebase.auth().onAuthStateChanged(function (user) {
+		var firebasePlayersArr = $firebaseArray(playersRef);
 
-    firebase.auth().onAuthStateChanged(function (user) {
-        var firebasePlayersArr = $firebaseArray(playersRef)
-        firebasePlayersArr.$loaded().then(function (data) {
-            var FBplayers = data;
-            console.log("players", FBplayers)
-            if (user) {
-                var userAuthId = user.uid;
-                var me = $scope.FBplayers.filter(player => player.uid === userAuthId)[0];
-                console.log("me idx", FBplayers.indexOf(me))
-                if (me) $scope.me = me;
-                if ($scope.me.marker === "n") $scope.me.marker = null;
+		firebasePlayersArr.$loaded().then(function (data) {
+			var FBplayers = data;
 
-            } else {
-                // No user is signed in.
-                console.log("nothing")
-            }
-        })
-    });
+			if (user) {
+				var userAuthId = user.uid;
+				var me = FBplayers.filter(player => player.uid === userAuthId)[0];
+				if (me) $scope.me = me;
+				if ($scope.me.marker === "n") $scope.me.marker = null;
+			} else {
+				// No user is signed in.
+				console.log("nothing");
+			}
+		});
+	});
 
-    //Have player pick the marker
-    $scope.pickMarker = function (board, marker) {
-        $scope.me.marker = marker;
-        var firebasePlayersArr = $firebaseArray(playersRef)
-        firebasePlayersArr.$loaded()
-            .then(function (players) {
-                var meIdx = players.indexOf($scope.me)
-                firebasePlayersArr.$save(idx)
+	$scope.pickMarker = function (board, marker) {
+		$scope.me.marker = marker;
+		var firebasePlayersArr = $firebaseArray(playersRef);
 
-            })
-        var idx = $scope.availableMarkers.indexOf(marker);
-        $scope.availableMarkers.splice(idx, 1)
-        markersArr.$save(0)
-            .then(function (ref) {
-                console.log("removed the picked one")
-                console.log(ref.key)
-            })
-    };
+		firebasePlayersArr.$loaded()
+			.then(function (players) {
+				var meIdx;
 
-    //Have player pick their start point
+				players.find(function(e,i){
+					if(e.$id === $scope.me.$id) meIdx = i;
+				});
 
-    $scope.placeMarker = function (board, point) {
-        $scope.me.placeMarker(point);
-        $scope.game.players.push($scope.player);
+				firebasePlayersArr[meIdx].marker = marker;
+				firebasePlayersArr.$save(meIdx);
+			});
 
-        gameRef.child('players').child(player.uid).push({
-            'marker': player.marker,
-            'startingPosition': player.startingPosition
-        });
-    };
+		var idx = $scope.game.availableMarkers.indexOf(marker);
 
-    //take all players on firebase and turn them into local player
-    playersRef.on("child_added", function (player) {
-        var newPlayer = new Player(player.uid);
-        newPlayer.marker = player.marker;
+		$scope.game.availableMarkers.splice(idx, 1);
+		markersArr[0].splice(idx,1);
 
-        var x = player.startingPosition[0];
-        var y = player.startingPosition[1];
-        var pointsIndex = player.startingPosition[2];
+		markersArr.$save(0)
+			.then(function (ref) {
+				console.log("removed the picked marker");
+				console.log(ref.key);
+			});
+	};
 
-        newPlayer.point = board[y][x].points[pointsIndex];
-        newPlayer.nextSpace = board[y][x];
-        newPlayer.nextSpacePointsIndex = player.startingPosition[2];
+/////K: working up until here!
 
-        newPlayer.tiles = $scope.game.deck.dealThree();
+	    //Have player pick their start point
 
-        $scope.game.players.push(newPlayer);
-    });
+	    $scope.placeMarker = function (board, point) {
+	        $scope.me.placeMarker(point);
+	        $scope.game.players.push($scope.player);
 
+	        gameRef.child('players').child(player.uid).push({
+	            'marker': player.marker,
+	            'startingPosition': player.startingPosition
+	        });
+	    };
 
+	    //take all players on firebase and turn them into local player
+	    playersRef.on("child_added", function (player) {
+	        var newPlayer = new Player(player.uid);
+	        newPlayer.marker = player.marker;
 
+	        var x = player.startingPosition[0];
+	        var y = player.startingPosition[1];
+	        var pointsIndex = player.startingPosition[2];
 
+	        newPlayer.point = board[y][x].points[pointsIndex];
+	        newPlayer.nextSpace = board[y][x];
+	        newPlayer.nextSpacePointsIndex = player.startingPosition[2];
 
+	        newPlayer.tiles = $scope.game.deck.dealThree();
 
-
-    // TODO: we probably need this on firebase so other people can't pick what's been picked
-
-    //For synchronizingGame...
-    var syncRef = gameRef.child('moves');
-    syncRef.on('child_added', function (childSnapshot, prevChildKey) {
-        //NEED TO DOUBLE CHECK!! What does childSnap returns?
-        console.log('childSnapshot_SyncGame', childSnapshot);
-        //depending on what childSnapshot gives me...I think it's one child per on call? It doesn't return an array of changes...I believe!
-        if (childSnapshot.type === 'updateDeck') {
-            $scope.game.deck = childSnapshot.updateDeck;
-        } else {
-            $scope.placeTile(childSnapshot.tile);
-        }
-    });
-
-    // TODO: how to re-do the moves?
-    // $scope.game.moves;
-
-    // TODO: how do we show the tiles for player?
-
-    // TODO: how to show the rotated tile?
-
-    // CMT: assuming we use new Game() for each game
-    $scope.currentPlayer = $scope.game.getCurrentPlayer();
-
-    // CMT: assuming we use new Game() for each game, holds all the players still on the board.
-    // $scope.turnOrderArray = $scope.game.getCanPlay();
-
-    // TODO: need a function to assign dragon
-    $scope.dragon;
-    var awaitingDragonHolders = [];
+	        $scope.game.players.push(newPlayer);
+	    });
 
 
-    $scope.start = function () {
-        //
-    };
-
-    $scope.myTurn = function () {
-        $scope.me === $scope.currentPlayer;
-    };
-
-    //these are tied to angular ng-click buttons
-    $scope.rotateTileCw = function (tile) {
-        console.log("rotate to right")
-        tile.rotation++;
-        if (tile.rotation === 4) tile.rotation = 0;
-    };
-
-    $scope.rotateTileCcw = function (tile) {
-        tile.rotation--;
-        if (tile.rotation === -4) tile.rotation = 0;
-    };
-
-    // CMT: assuming we use new Game()
-    // CMT: use player's and game's prototype function to place tile and then move all players
-    $scope.placeTile = function (tile) {
-        // TODO: send this state to firebase every time it's called
-        if (tile.rotation > 0) {
-            tile.paths = tile.paths.map(function (connection) {
-                return connection + 2;
-            });
-            tile.paths.unshift(tile.paths.pop());
-            tile.paths.unshift(tile.paths.pop());
-        } else if (tile.rotation < 0) {
-            tile.paths = tile.paths.map(function (connection) {
-                return connection - 2;
-            });
-            tile.paths.push(tile.paths.shift());
-            tile.paths.push(tile.paths.shift());
-        }
-
-        $scope.me.placeTile(tile);
-        gameRef.child('moves').$add({
-            'type': 'placeTile',
-            'tile': tile
-        });
-
-        $scope.game.moveAllplayers();
-
-        if ($scope.game.checkOver()) {
-            // TODO: need to tell the player she won
-            $scope.winner = $scope.game.getCanPlay()[0];
-            $scope.gameOver = true;
-        } else {
-            // If deck is empty & no one is dragon, set me as dragon
-            if ($scope.game.deck.length === 0 && !$scope.dragon) {
-                $scope.dragon = $scope.me;
-            } else if ($scope.game.deck.length === 0 && $scope.dragon) {
-                awaitingDragonHolders.push($scope.me);
-            } else {
-                // CMT: draw one tile and push it to the player.tiles array
-                $scope.me.tiles.push($scope.game.deck.deal(1));
-                //if dead players, then push their cards back to the deck & reshuffle
-                if ($scope.game.deadPlayers().length) {
-                    //with new cards & need to reshuffle
-                    $scope.game.deadPlayers().forEach(function (deadPlayerTiles) {
-                        deadPlayerTiles.forEach(function (tile) {
-                            $scope.game.deck.push(tile);
-                        });
-                    });
-                    $scope.game.deck = $scope.game.deck.shuffle();
-                    //send firebase a new move
-                    gameRef.child('moves').push({
-                        'type': 'updateDeck',
-                        'updateDeck': $scope.game.deck
-                    });
-                    if ($scope.dragon) {
-                        $scope.dragon.tiles.push($scope.game.deck.deal(1));
-                        $scope.dragon = null;
-                        //NEED TO DISCUSS: Might need to modify this if we want to use up the cards and give each awaiting players' up to 3 cards
-                        while ($scope.game.deck.length && $scope.awaitingDragonHolders.length) {
-                            $scope.awaitingDragonHolders.shift().tiles.push($scope.game.deck.deal(1));
-                        };
-                        if ($scope.awaitingDragonHolders.length) {
-                            $scope.dragon = $scope.awaitingDragonHolders.shift();
-                        }
-                    };
-                }
-
-            }
-            $scope.game.goToNextPlayer();
-        }
-    };
-
-    // TODO: firebase game.players slice $scope.player out
-    $scope.leaveGame;
-
-    // TODO: do we remove this game room's moves from firebase?
-    $scope.reset = $scope.game.reset;
 
 
-    $scope.starttop = [
-        [0, 0, 0],
-        [0, 0, 1],
-        [1, 0, 0],
-        [1, 0, 1],
-        [2, 0, 0],
-        [2, 0, 1],
-        [3, 0, 0],
-        [3, 0, 1],
-        [4, 0, 0],
-        [4, 0, 1],
-        [5, 0, 0],
-        [5, 0, 1]
-    ];
-    $scope.startleft = [
-        [0, 0, 7],
-        [0, 0, 6],
-        [0, 1, 7],
-        [0, 1, 6],
-        [0, 2, 7],
-        [0, 2, 6],
-        [0, 3, 7],
-        [0, 3, 6],
-        [0, 4, 7],
-        [0, 4, 6],
-        [0, 5, 7],
-        [0, 5, 6]
-    ];
-    $scope.startbottom = [
-        [0, 5, 0],
-        [0, 5, 1],
-        [1, 5, 0],
-        [1, 5, 1],
-        [2, 5, 0],
-        [2, 5, 1],
-        [3, 5, 0],
-        [3, 5, 1],
-        [4, 5, 0],
-        [4, 5, 1],
-        [5, 5, 0],
-        [5, 5, 1]
-    ];
-    $scope.startright = [
-        [5, 0, 2],
-        [5, 0, 3],
-        [5, 1, 2],
-        [5, 1, 3],
-        [5, 2, 2],
-        [5, 2, 3],
-        [5, 3, 2],
-        [5, 3, 3],
-        [5, 4, 2],
-        [5, 4, 3],
-        [5, 5, 2],
-        [5, 5, 3]
-    ];
 
-});;
+
+
+	    // TODO: we probably need this on firebase so other people can't pick what's been picked
+
+	    //For synchronizingGame...
+	    var syncRef = gameRef.child('moves');
+	    syncRef.on('child_added', function (childSnapshot, prevChildKey) {
+	        //NEED TO DOUBLE CHECK!! What does childSnap returns?
+	        console.log('childSnapshot_SyncGame', childSnapshot);
+	        //depending on what childSnapshot gives me...I think it's one child per on call? It doesn't return an array of changes...I believe!
+	        if (childSnapshot.type === 'updateDeck') {
+	            $scope.game.deck = childSnapshot.updateDeck;
+	        } else {
+	            $scope.placeTile(childSnapshot.tile);
+	        }
+	    });
+
+	    // TODO: how to re-do the moves?
+	    // $scope.game.moves;
+
+	    // TODO: how do we show the tiles for player?
+
+	    // TODO: how to show the rotated tile?
+
+	    // CMT: assuming we use new Game() for each game
+	    $scope.currentPlayer = $scope.game.getCurrentPlayer();
+
+	    // CMT: assuming we use new Game() for each game, holds all the players still on the board.
+	    // $scope.turnOrderArray = $scope.game.getCanPlay();
+
+	    // TODO: need a function to assign dragon
+	    $scope.dragon;
+	    var awaitingDragonHolders = [];
+
+
+	    $scope.start = function () {
+	        //
+	    };
+
+	    $scope.myTurn = function () {
+	        $scope.me === $scope.currentPlayer;
+	    };
+
+	    //these are tied to angular ng-click buttons
+	    $scope.rotateTileCw = function (tile) {
+	        console.log("rotate to right")
+	        tile.rotation++;
+	        if (tile.rotation === 4) tile.rotation = 0;
+	    };
+
+	    $scope.rotateTileCcw = function (tile) {
+	        tile.rotation--;
+	        if (tile.rotation === -4) tile.rotation = 0;
+	    };
+
+	    // CMT: assuming we use new Game()
+	    // CMT: use player's and game's prototype function to place tile and then move all players
+	    $scope.placeTile = function (tile) {
+	        // TODO: send this state to firebase every time it's called
+	        if (tile.rotation > 0) {
+	            tile.paths = tile.paths.map(function (connection) {
+	                return connection + 2;
+	            });
+	            tile.paths.unshift(tile.paths.pop());
+	            tile.paths.unshift(tile.paths.pop());
+	        } else if (tile.rotation < 0) {
+	            tile.paths = tile.paths.map(function (connection) {
+	                return connection - 2;
+	            });
+	            tile.paths.push(tile.paths.shift());
+	            tile.paths.push(tile.paths.shift());
+	        }
+
+	        $scope.me.placeTile(tile);
+	        gameRef.child('moves').$add({
+	            'type': 'placeTile',
+	            'tile': tile
+	        });
+
+	        $scope.game.moveAllplayers();
+
+	        if ($scope.game.checkOver()) {
+	            // TODO: need to tell the player she won
+	            $scope.winner = $scope.game.getCanPlay()[0];
+	            $scope.gameOver = true;
+	        } else {
+	            // If deck is empty & no one is dragon, set me as dragon
+	            if ($scope.game.deck.length === 0 && !$scope.dragon) {
+	                $scope.dragon = $scope.me;
+	            } else if ($scope.game.deck.length === 0 && $scope.dragon) {
+	                awaitingDragonHolders.push($scope.me);
+	            } else {
+	                // CMT: draw one tile and push it to the player.tiles array
+	                $scope.me.tiles.push($scope.game.deck.deal(1));
+	                //if dead players, then push their cards back to the deck & reshuffle
+	                if ($scope.game.deadPlayers().length) {
+	                    //with new cards & need to reshuffle
+	                    $scope.game.deadPlayers().forEach(function (deadPlayerTiles) {
+	                        deadPlayerTiles.forEach(function (tile) {
+	                            $scope.game.deck.push(tile);
+	                        });
+	                    });
+	                    $scope.game.deck = $scope.game.deck.shuffle();
+	                    //send firebase a new move
+	                    gameRef.child('moves').push({
+	                        'type': 'updateDeck',
+	                        'updateDeck': $scope.game.deck
+	                    });
+	                    if ($scope.dragon) {
+	                        $scope.dragon.tiles.push($scope.game.deck.deal(1));
+	                        $scope.dragon = null;
+	                        //NEED TO DISCUSS: Might need to modify this if we want to use up the cards and give each awaiting players' up to 3 cards
+	                        while ($scope.game.deck.length && $scope.awaitingDragonHolders.length) {
+	                            $scope.awaitingDragonHolders.shift().tiles.push($scope.game.deck.deal(1));
+	                        };
+	                        if ($scope.awaitingDragonHolders.length) {
+	                            $scope.dragon = $scope.awaitingDragonHolders.shift();
+	                        }
+	                    };
+	                }
+
+	            }
+	            $scope.game.goToNextPlayer();
+	        }
+	    };
+
+	    // TODO: firebase game.players slice $scope.player out
+	    $scope.leaveGame;
+
+	    // TODO: do we remove this game room's moves from firebase?
+	    $scope.reset = $scope.game.reset;
+
+
+	    $scope.starttop = [
+	        [0, 0, 0],
+	        [0, 0, 1],
+	        [1, 0, 0],
+	        [1, 0, 1],
+	        [2, 0, 0],
+	        [2, 0, 1],
+	        [3, 0, 0],
+	        [3, 0, 1],
+	        [4, 0, 0],
+	        [4, 0, 1],
+	        [5, 0, 0],
+	        [5, 0, 1]
+	    ];
+	    $scope.startleft = [
+	        [0, 0, 7],
+	        [0, 0, 6],
+	        [0, 1, 7],
+	        [0, 1, 6],
+	        [0, 2, 7],
+	        [0, 2, 6],
+	        [0, 3, 7],
+	        [0, 3, 6],
+	        [0, 4, 7],
+	        [0, 4, 6],
+	        [0, 5, 7],
+	        [0, 5, 6]
+	    ];
+	    $scope.startbottom = [
+	        [0, 5, 0],
+	        [0, 5, 1],
+	        [1, 5, 0],
+	        [1, 5, 1],
+	        [2, 5, 0],
+	        [2, 5, 1],
+	        [3, 5, 0],
+	        [3, 5, 1],
+	        [4, 5, 0],
+	        [4, 5, 1],
+	        [5, 5, 0],
+	        [5, 5, 1]
+	    ];
+	    $scope.startright = [
+	        [5, 0, 2],
+	        [5, 0, 3],
+	        [5, 1, 2],
+	        [5, 1, 3],
+	        [5, 2, 2],
+	        [5, 2, 3],
+	        [5, 3, 2],
+	        [5, 3, 3],
+	        [5, 4, 2],
+	        [5, 4, 3],
+	        [5, 5, 2],
+	        [5, 5, 3]
+	    ];
+});
