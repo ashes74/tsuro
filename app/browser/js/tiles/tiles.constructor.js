0e9f4f0111e69b7da9a0b592ee92dddb2105e945
Lori-Anne Ashwood <ashes74@users.noreply.github.com>
2016-07-01 13:22:49 -0400

    Tabling Tile refactor

--- app/browser/js/tiles/tiles.constructor.js
+++ app/browser/js/tiles/tiles.constructor.js
@@ -1,14 +1,14 @@
-'use strict';
-
-//TODO: rewrite this and put in the game.js place Tile function
-var tiles = require('../tiles.js');
-
-class Tile {
-	constructor(tile) {
-		this = tile;
-	}
-
-	rotate(){
-
-	}
-}
+// 'use strict';
+//
+// //TODO: rewrite this and put in the game.js place Tile function
+// var tiles = require('../tiles.js');
+//
+// class Tile {
+// 	constructor(tile) {
+// 		this = tile;
+// 	}
+//
+// 	rotate(){
+//
+// 	}
+// }
