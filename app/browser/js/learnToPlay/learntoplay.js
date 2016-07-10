1d09c903f3e98530d53e9f1b8cc7bd20b6e79986
Kimber Lynch <kmlynch714@gmail.com>
2016-07-10 14:01:57 -0400

    initial learn to play html

---
+++ app/browser/js/learnToPlay/learntoplay.js
@@ -0,0 +1,6 @@
+tsuro.config(function ($stateProvider) {
+    $stateProvider.state('learntoplay', {
+        url: '/learntoplay',
+        templateUrl: '/browser/js/learnToPlay/learntoplay.html',
+    });
+});
